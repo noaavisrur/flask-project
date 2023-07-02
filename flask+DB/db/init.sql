f47832369e2859ee408af1f0d739935d3aa0f763
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-02 10:20:25 +0300

    Create init.sql

---
+++ flask+DB/db/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE devopsroles;
+USE devopsroles;
+
+CREATE TABLE test_table (
+  url VARCHAR(100)
+);
+
+INSERT INTO test_table (url)
+VALUES
+  ("https://media.tenor.com/OZLXGyrE6FYAAAAM/alpaca-eating.gif"),
+  ("https://media0.giphy.com/media/3otWpNrbfVfVgyAOis/200w.gif?cid=6c0");

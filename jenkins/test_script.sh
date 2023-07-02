4ff6a3eee3f892970872504dc6422668c2723c1c
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-02 14:32:09 +0300

    Update test_script.sh

--- jenkins/test_script.sh
+++ jenkins/test_script.sh
@@ -4,8 +4,7 @@ port=5000  # Replace with the actual port number of your Flask server
 # Make HTTP request to Flask server and capture response
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$port)
 
-# Print response
-echo "HTTP response code: ${response}"
+
 
 # Check if response code indicates success or failure
 if [ "${response}" == "200" ]; then

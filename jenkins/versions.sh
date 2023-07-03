2a9e5a65041b14dac62123f0944a939104ab2080
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-03 10:53:01 +0300

    Update versions.sh

--- jenkins/versions.sh
+++ jenkins/versions.sh
@@ -1,6 +1,23 @@
-!#/usr/bin/python3
+#!/usr/bin/python3
 
 import docker
+
+def delete_old_versions(image_name, keep_latest=5):
+    client = docker.from_env()
+    images = client.images.list(name=image_name)
+
+    if len(images) <= keep_latest:
+        return
+
+    images.sort(key=lambda image: image.attrs["Created"], reverse=True)
+
+    for i in range(keep_latest, len(images)):
+        image = images[i]
+        for tag in image.tags:
+            if tag.startswith(image_name + ":"):
+                print(f"Deleting image: {tag}")
+                client.images.remove(image=tag, force=True)
+
 client = docker.from_env()
 images = client.images.list()
 existing_versions = [float(image.tags[0].split(":")[1]) for image in images if image.tags and image.tags[0].startswith("noaavisrur/flask-app:")]
@@ -9,12 +26,12 @@ if existing_versions:
     next_version = latest_version + 0.1
 else:
     next_version = 1.0
-# Format the version number to one decimal place
+
 next_version = f"{next_version:.1f}"
 image_name = f"noaavisrur/flaskapp:{next_version}"
 client.images.build(path=".", tag=image_name, rm=True, pull=True)
 print(f"Successfully built image: {image_name}")
-# Push the image to Docker Hub
 client.images.push(repository="noaavisrur/flask-app", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
-client.images.push(repository="noaavisrur/flask-app", tag="latest")
+
+delete_old_versions("noaavisrur/flask-app", keep_latest=5)

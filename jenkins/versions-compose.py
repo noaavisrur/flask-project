96d03a14708b919c04563d4c5de46fd595b6729b
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-09 13:31:59 +0300

    Update versions-compose.py

--- jenkins/versions-compose.py
+++ jenkins/versions-compose.py
@@ -34,12 +34,18 @@ print(f"Successfully built image: {image_name}")
 client.images.push(repository="noaavisrur/flask-compose", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
 # Tag the next version as "latest"
-
 latest_image_name = "noaavisrur/flask-compose:latest"
 client.images.get(image_name).tag(latest_image_name)
 print(f"Successfully tagged image as latest: {latest_image_name}")
 # Push the latest image to Docker Hub
 client.images.push(repository="noaavisrur/flask-compose", tag="latest")
 print(f"Successfully pushed latest image: {latest_image_name}")
+
+# Building and pushing MySQL image with the "latest" tag
+mysql_image_name = "noaavisrur/mysql-compose:latest"
+client.images.build(path="/var/lib/jenkins/workspace/docker_compose_flask/flask-project/flask+DB/db", tag=mysql_image_name, rm=True, pull=True)
+print(f"Successfully built MySQL image: {mysql_image_name}")
+client.images.push(repository="noaavisrur/mysql-compose", tag="latest")
+print(f"Successfully pushed MySQL image: {mysql_image_name}")
 
 delete_old_versions("noaavisrur/flask-compose", keep_latest=5)

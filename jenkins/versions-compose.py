e81ecc414c3e1b1690c178b806de91d3368091ed
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-16 13:15:23 +0300

    Update versions-compose.py

--- jenkins/versions-compose.py
+++ jenkins/versions-compose.py
@@ -18,42 +18,42 @@ def delete_old_versions(image_name, keep_latest=5):
                 print(f"Deleting image: {tag}")
                 client.images.remove(image=tag, force=True)
 
-try:
-    client = docker.from_env()
-    images = client.images.list()
-    existing_versions = [float(image.tags[0].split(":")[1]) for image in images if image.tags and image.tags[0].startswith("noaavisrur/flask-app:")]
+client = docker.from_env()
+images = client.images.list()
 
-    print(f"Existing versions: {existing_versions}")  # Debugging line
+left_versions = []
+right_versions = []
+for image in images:
+    if image.tags and image.tags[0].startswith("noaavisrur/flask-app:"):
+        left, right = map(int, image.tags[0].split(":")[1].split("."))
+        left_versions.append(left)
+        right_versions.append(right)
 
-    if existing_versions:
-        latest_version = max(existing_versions)
-        next_version = latest_version + 0.1
-    else:
-        next_version = 1.0
+if left_versions:
+    latest_left_version = max(left_versions)
+    latest_right_version = max(right_versions)
+    next_version = f"{latest_left_version}.{latest_right_version + 1}"
+else:
+    next_version = "1.0"
 
-    next_version = f"{next_version:.1f}"
-    image_name = f"noaavisrur/flask-compose:{next_version}"
-    print(f"Next version: {next_version}")  # Debugging line
-    client.images.build(path="/var/lib/jenkins/workspace/docker_compose_flask/flask-project/flask+DB/flask-app", tag=image_name, rm=True, pull=True)
-    print(f"Successfully built image: {image_name}")
-    client.images.push(repository="noaavisrur/flask-compose", tag=next_version)
-    print(f"Successfully pushed image: {image_name}")
-    # Tag the next version as "latest"
-    latest_image_name = "noaavisrur/flask-compose:latest"
-    client.images.get(image_name).tag(latest_image_name)
-    print(f"Successfully tagged image as latest: {latest_image_name}")
-    # Push the latest image to Docker Hub
-    client.images.push(repository="noaavisrur/flask-compose", tag="latest")
-    print(f"Successfully pushed latest image: {latest_image_name}")
+image_name = f"noaavisrur/flask-compose:{next_version}"
+client.images.build(path="/var/lib/jenkins/workspace/docker_compose_flask/flask-project/flask+DB/flask-app", tag=image_name, rm=True, pull=True)
+print(f"Successfully built image: {image_name}")
+client.images.push(repository="noaavisrur/flask-compose", tag=next_version)
+print(f"Successfully pushed image: {image_name}")
+# Tag the next version as "latest"
+latest_image_name = "noaavisrur/flask-compose:latest"
+client.images.get(image_name).tag(latest_image_name)
+print(f"Successfully tagged image as latest: {latest_image_name}")
+# Push the latest image to Docker Hub
+client.images.push(repository="noaavisrur/flask-compose", tag="latest")
+print(f"Successfully pushed latest image: {latest_image_name}")
 
-    # Building and pushing MySQL image with the "latest" tag
-    mysql_image_name = "noaavisrur/mysql-compose:latest"
-    client.images.build(path="/var/lib/jenkins/workspace/docker_compose_flask/flask-project/flask+DB/db", tag=mysql_image_name, rm=True, pull=True)
-    print(f"Successfully built MySQL image: {mysql_image_name}")
-    client.images.push(repository="noaavisrur/mysql-compose", tag="latest")
-    print(f"Successfully pushed MySQL image: {mysql_image_name}")
+# Building and pushing MySQL image with the "latest" tag
+mysql_image_name = "noaavisrur/mysql-compose:latest"
+client.images.build(path="/var/lib/jenkins/workspace/docker_compose_flask/flask-project/flask+DB/db", tag=mysql_image_name, rm=True, pull=True)
+print(f"Successfully built MySQL image: {mysql_image_name}")
+client.images.push(repository="noaavisrur/mysql-compose", tag="latest")
+print(f"Successfully pushed MySQL image: {mysql_image_name}")
 
-    delete_old_versions("noaavisrur/flask-compose", keep_latest=5)
-
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+delete_old_versions("noaavisrur/flask-compose", keep_latest=5)

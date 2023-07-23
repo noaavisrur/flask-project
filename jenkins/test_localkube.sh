60e16f09c18c4b35ac5bbfe6af05c59d42b6caf5
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-07-23 14:24:49 +0300

    Update test_localkube.sh

--- jenkins/test_localkube.sh
+++ jenkins/test_localkube.sh
@@ -29,6 +29,7 @@ if [ -z "$port" ]; then
 fi
 
 # Make HTTP request to the Kubernetes service using the Windows machine's IP and the dynamically assigned LoadBalancer port (ClusterIP:Port)
+echo $windows_machine_ip:$port
 response=$(curl -s -o /dev/null -w "%{http_code}" http://$windows_machine_ip:$port)
 
 # Check if response code indicates success or failure

b5a22a324139c40c5550e43d5aa7bffa4bd0ee06
Noa Avisrur <133655291+noaavisrur@users.noreply.github.com>
2023-09-11 01:30:16 +0300

    Update versions-helm.py

--- jenkins/versions-helm.py
+++ jenkins/versions-helm.py
@@ -45,4 +45,25 @@ else:
 
 next_version = f"{next_version_left}.{next_version_right}"
 
-# Your build, push, and clean-up code can follow here
+image_name = f"noaavisrur/flask-compose:{next_version}"
+client.images.build(path="/var/lib/jenkins/workspace/helm_pipeline/flask-project/flask+DB/flask-app", tag=image_name, rm=True, pull=True)
+print(f"Successfully built image: {image_name}")
+client.images.push(repository="noaavisrur/flask-compose", tag=next_version)
+print(f"Successfully pushed image: {image_name}")
+# Tag the next version as "latest"
+latest_image_name = "noaavisrur/flask-compose:latest"
+client.images.get(image_name).tag(latest_image_name)
+print(f"Successfully tagged image as latest: {latest_image_name}")
+# Push the latest image to Docker Hub
+client.images.push(repository="noaavisrur/flask-compose", tag="latest")
+print(f"Successfully pushed latest image: {latest_image_name}")
+
+# Building and pushing MySQL image with the "latest" tag
+mysql_image_name = "noaavisrur/mysql-compose:latest"
+client.images.build(path="/var/lib/jenkins/workspace/helm_pipeline/flask-project/flask+DB/db", tag=mysql_image_name, rm=True, pull=True)
+print(f"Successfully built MySQL image: {mysql_image_name}")
+client.images.push(repository="noaavisrur/mysql-compose", tag="latest")
+print(f"Successfully pushed MySQL image: {mysql_image_name}")
+
+delete_old_versions("noaavisrur/flask-compose", keep_latest=5)
+
